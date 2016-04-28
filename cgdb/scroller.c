4e6b7abb7a690b265825fad3acfaf0fd8e900b32
Michael Sartain <mikesart@gmail.com>
2016-04-28 07:25:13 -0700

    Add scroll mode to GDB Window.
    Hitting PgUp will start scrolling as it did before, but now you will
    also be put in scroll mode, which will use up/down arrows and
    ctrl_p/ctrl_n to scroll by single lines. When you return to bottom of
    the buffer or hit F12 (scr_end), scroll mode exits.
    
    Signed-off-by: Michael Sartain <mikesart@gmail.com>

--- cgdb/scroller.c
+++ cgdb/scroller.c
@@ -125,6 +125,7 @@ struct scroller *scr_new(int pos_r, int pos_c, int height, int width)
     rv->current.r = 0;
     rv->current.c = 0;
     rv->current.pos = 0;
+    rv->in_scroll_mode = 0;
     rv->win = newwin(height, width, pos_r, pos_c);
 
     /* Start with a single (blank) line */
@@ -156,6 +157,8 @@ void scr_up(struct scroller *scr, int nlines)
     int height, width;
     int length;
     int i;
+
+    scr->in_scroll_mode = 1;
 
     /* Sanity check */
     getmaxyx(scr->win, height, width);
@@ -189,6 +192,8 @@ void scr_down(struct scroller *scr, int nlines)
     int length;
     int i;
 
+    int at_bottom = 0;
+
     /* Sanity check */
     getmaxyx(scr->win, height, width);
     if (scr->current.c > 0) {
@@ -209,16 +214,22 @@ void scr_down(struct scroller *scr, int nlines)
                 scr->current.c = 0;
             } else {
                 /* At bottom */
+                at_bottom = 1;
                 break;
             }
         }
     }
+
+    if ( at_bottom )
+        scr->in_scroll_mode = 0;
 }
 
 void scr_home(struct scroller *scr)
 {
     scr->current.r = 0;
     scr->current.c = 0;
+
+    scr->in_scroll_mode = 1;
 }
 
 void scr_end(struct scroller *scr)
@@ -229,6 +240,8 @@ void scr_end(struct scroller *scr)
 
     scr->current.r = scr->length - 1;
     scr->current.c = (strlen(scr->buffer[scr->current.r]) / width) * width;
+
+    scr->in_scroll_mode = 0;
 }
 
 void scr_add(struct scroller *scr, const char *buf)
