aa0c72c37d84ead55805264ae7d5600a4af750b2
Michael Sartain <mikesart@gmail.com>
2016-05-20 19:16:18 -0700

    Add AmIBeingDebugged for Mac - not yet working.
    Signed-off-by: Michael Sartain <mikesart@gmail.com>

--- lib/util/sys_util.cpp
+++ lib/util/sys_util.cpp
@@ -73,6 +73,51 @@ int cgdb_close(int fd)
     return 0;
 }
 
+#if defined( __APPLE__ )
+
+// https://developer.apple.com/library/mac/qa/qa1361/_index.html
+
+#include <assert.h>
+#include <stdbool.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/sysctl.h>
+
+static bool AmIBeingDebugged(void)
+    // Returns true if the current process is being debugged (either 
+    // running under the debugger or has a debugger attached post facto).
+{
+    int                 junk;
+    int                 mib[4];
+    struct kinfo_proc   info;
+    size_t              size;
+
+    // Initialize the flags so that, if sysctl fails for some bizarre 
+    // reason, we get a predictable result.
+
+    info.kp_proc.p_flag = 0;
+
+    // Initialize mib, which tells sysctl the info we want, in this case
+    // we're looking for information about a specific process ID.
+
+    mib[0] = CTL_KERN;
+    mib[1] = KERN_PROC;
+    mib[2] = KERN_PROC_PID;
+    mib[3] = getpid();
+
+    // Call sysctl.
+
+    size = sizeof(info);
+    junk = sysctl(mib, sizeof(mib) / sizeof(*mib), &info, &size, NULL, 0);
+    assert(junk == 0);
+
+    // We're being debugged if the P_TRACED flag is set.
+
+    return ( (info.kp_proc.p_flag & P_TRACED) != 0 );
+}
+
+#endif // __APPLE__
+
 int cgdb_is_debugger_attached()
 {
 #ifdef HAVE_PROC_SELF_STATUS_FILE
@@ -99,8 +144,10 @@ int cgdb_is_debugger_attached()
     }
 
     return debugger_attached;
+#elif defined( __APPLE__ )
+    return AmIBeingDebugged();
 #else
-    /* TODO: Implement for other platforms. */
+    //$ TODO
     return -1;
 #endif
 }
