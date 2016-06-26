627e281ec2791697e7728d1a898eb343f041a12b
Michael Sartain <mikesart@gmail.com>
2016-06-26 08:40:57 -0700

    Qtcreator project files generator shell script.
    Signed-off-by: Michael Sartain <mikesart@gmail.com>

---
+++ qtcreator/update_files.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+PROJNAME=cgdb
+
+BASEDIR=$(dirname $0)
+cd $BASEDIR
+
+DIRS=(..)
+
+# Create blah.creator file if it doesn't exist
+if [[ ! -f ${PROJNAME}.creator ]]; then echo -e "[General]\n" > ${PROJNAME}.creator; fi
+
+# Create our defines file
+DEFINES=$(cat <<'END_HEREDOC'
+#define __LINUX__ 1
+#define _GNU_SOURCE 1
+#define HAVE_CONFIG_H 1
+#define HAVE_CURSES_H 1
+#define HAVE_LIBREADLINE 1
+#define HAVE_READLINE_HISTORY_H 1
+#define NCURSES_VERSION 1
+#define CLOG_MAIN 1
+
+END_HEREDOC
+)
+echo "${DEFINES}" > ${PROJNAME}.config
+
+> ${PROJNAME}.files
+for i in ${DIRS[@]}; do
+    echo Checking ${i}
+
+    find ${i} -type f -iregex '.*\.\(c\|cxx\|cpp\|h\|lpp\|ypp\|sh\|txt\)$' -or -iname Makefile >> ${PROJNAME}.files
+done
+
+find /usr/include -iname curses.h >> ${PROJNAME}.files
+
+fgrep -i --color=never ".h" *.files  | xargs -I{} dirname {} | sort | uniq > ${PROJNAME}.includes
+
+wc -l ${PROJNAME}.files
+wc -l ${PROJNAME}.includes
